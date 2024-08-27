021fc42a5a6ac929714412a3495c206060a7508f
allanDev9 <allanpenaguerra2003@gmail.com>
2024-08-27 00:19:14 -0600

    Use to useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,41 @@ import './App.css'
 
 function App() {
   const [enable, setEnable] = useState(false)
+  const [position,setPositio] = useState({x: 0, y:0})
 
   useEffect(() =>{
-    
-  })
+    console.log('efect', { enable })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY})
+    }
+    if (enable){
+      window.addEventListener('pointermove', handleMove)
+    }
+
+  }, [enable])
 
   return (
-    <>
-    <h1>Projects 3</h1>
+    <main>
+    <div style={{
+      position: 'absolute',
+      backgroundColor: '#09f',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height: 40,
+      transform: 'traslate(opx, 0px)'
+    }}>
+
+    </div>
     <button onClick={() => setEnable(!enable)}>
       {enable ? 'Desactivar' : 'Activar'} seguir puntero
     </button>
-    </>
+    </main>
   )
 }
 
